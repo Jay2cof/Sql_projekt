9b825707c9802531b79527d7de5ea99eb251023c
Justice Chima Onuoha <45771491+Jay2cof@users.noreply.github.com>
2024-04-20 13:18:27 +0200

    added
    droping table

---
+++ subject.sql
@@ -0,0 +1,14 @@
+create database result;
+use result;
+Create Table subjects(
+score Int,
+grade varchar(9),
+course varchar(100),
+subject_name varchar (50)
+
+);
+select * from subjects;
+insert into subjects values (30, 'Fail', 'Chemistry','sam');
+Drop table 
+subjects;
+
